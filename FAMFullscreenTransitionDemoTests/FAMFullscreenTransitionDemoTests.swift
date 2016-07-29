2f9522a0ce25e40131edc70e8496ad6b969f8a8e
Kazuya Ueoka <k.ueoka@timers-inc.com>
2016-07-29 16:07:18 +0900

    Initial Commit

---
+++ FAMFullscreenTransitionDemoTests/FAMFullscreenTransitionDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  FAMFullscreenTransitionDemoTests.swift
+//  FAMFullscreenTransitionDemoTests
+//
+//  Created by Kazuya Ueoka on 2016/07/29.
+//  Copyright © 2016年 Timers-Inc. All rights reserved.
+//
+
+import XCTest
+@testable import FAMFullscreenTransitionDemo
+
+class FAMFullscreenTransitionDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
