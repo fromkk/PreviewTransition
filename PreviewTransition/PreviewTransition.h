62e4861a343a9b22e8089d2305cf625e437e47be
Kazuya Ueoka <k.ueoka@timers-inc.com>
2016-08-01 10:32:14 +0900

    adapt to Carthage

---
+++ PreviewTransition/PreviewTransition.h
@@ -0,0 +1,19 @@
+//
+//  PreviewTransition.h
+//  PreviewTransition
+//
+//  Created by Kazuya Ueoka on 2016/08/01.
+//  Copyright © 2016年 Timers-Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PreviewTransition.
+FOUNDATION_EXPORT double PreviewTransitionVersionNumber;
+
+//! Project version string for PreviewTransition.
+FOUNDATION_EXPORT const unsigned char PreviewTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PreviewTransition/PublicHeader.h>
+
+
